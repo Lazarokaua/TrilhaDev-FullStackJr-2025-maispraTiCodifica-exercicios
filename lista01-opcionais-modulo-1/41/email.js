4c0b112d6f3a8bd79dea1e051fb25935a3bfb46c
Lazarokaua <lazarokaua4@gmail.com>
2025-06-02 21:45:53 -0300

    feat(opcionais) exercicio 41 sobre email e objetos

---
+++ lista01-opcionais-modulo-1/41/email.js
@@ -0,0 +1,11 @@
+// 41. Dado o objeto pessoa com propriedades nome e idade, acesse e imprima o valor de idade. Adicione uma nova propriedade chamada email ao objeto pessoa que já possui nome e idade
+
+const pessoa = {
+    nome: "Lázaro",
+    idade: 20
+}
+
+pessoa.email = "teste@gmail.com"
+
+
+console.log(pessoa)
