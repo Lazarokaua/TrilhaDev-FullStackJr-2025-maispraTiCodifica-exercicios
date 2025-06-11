0cba3b48dbc5e5739c439da596744f43958b5b60
Lazarokaua <lazarokaua4@gmail.com>
2025-06-11 13:10:33 -0300

    feat: começando os estudos para implementar o exercício 06 memoization

---
+++ lista02-obrigatórios-modulo-1/06/memorization.js
@@ -0,0 +1,5 @@
+// Implemente function memoize(fn) que armazene em cache chamadas anteriores de fn (por argumentos), retornando resultados instantâneos em repetidas invocações.
+
+// Article for your read ->  https://addyosmani.com/blog/faster-javascript-memoization/
+
+
